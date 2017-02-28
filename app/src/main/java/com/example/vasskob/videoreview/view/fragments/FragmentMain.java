0420ab8d46e2b7fb5afd2b30861c6d800ad89c22
vasskob <vasskob@gmail.com>
2017-02-28 18:47:30 +0200

    Base structure

---
+++ app/src/main/java/com/example/vasskob/videoreview/view/fragments/FragmentMain.java
@@ -0,0 +1,24 @@
+package com.example.vasskob.videoreview.view.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vasskob.videoreview.R;
+
+
+public class FragmentMain extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/vasskob/videoreview/view/main/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.vasskob.videoreview.view.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vasskob.videoreview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
